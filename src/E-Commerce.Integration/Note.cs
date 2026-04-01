116360d8023cf850b94aec67c29a526e22989391
Asim <superasim98@gmail.com>
2026-04-01 15:39:49 +0400

    (Integration) add integration layer

---
+++ src/E-Commerce.Integration/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Integration
+{
+    public class Note
+    {
+        //Integration Layer = HOW we call external systems
+        //Integration Context = HOW the business manages those calls
+    }
+}
